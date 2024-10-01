e59d3b11fcbd01ae960a1a5adde8544500497b70
danush07 <danush.h@marmeto.com>
2024-10-01 15:22:08 +0530

    final commit on 01-10

--- assets/global.js
+++ assets/global.js
@@ -1976,3 +1976,44 @@ class LazySection extends HTMLElement {
 }
 
 customElements.define("lazy-section", LazySection);
+class JavaScriptInjectedSection extends HTMLElement {
+  constructor() {
+    super();
+    this.init();
+  }
+
+  init() {
+    // Getting the selectors for target locations
+    this.targetLocationSelectors = [
+      this.dataset["targetLocation"],
+      this.dataset["targetLocation2"],
+    ].filter((selector) => selector);
+
+    this.targetLocations = this.targetLocationSelectors.map((selector) =>
+      document.querySelector(selector)
+    );
+
+    this.targetLocations.forEach((targetLocation) => {
+      if (targetLocation) {
+        targetLocation.innerHTML = this.innerHTML;
+      }
+    });
+  }
+}
+
+customElements.define("javascript-injected-section", JavaScriptInjectedSection);
+
+class JavaScriptInjectedSectionHamburger extends JavaScriptInjectedSection {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    document.querySelector("header-drawer").bindEvents();
+  }
+}
+
+customElements.define(
+  "javascript-injected-section-hamburger",
+  JavaScriptInjectedSectionHamburger
+);
